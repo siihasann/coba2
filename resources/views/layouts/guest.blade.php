2e07c421b6c9d4d57c3eff611db4cbc770cf5ad9
siihasann <hasannajib3@gmail.com>
2024-09-16 22:22:32 +0700

    updating auth view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link href="{{ asset('build/assets/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('build/assets/app.js') }}"></script>
+
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
