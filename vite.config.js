d521d251c85ed90c4b2e8e65ae5dbb979b611ddc
HASAN NAJIB <mashasanr@gmail.com>
2024-09-16 23:41:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build/assets', // Pastikan output path sesuai
+        outDir: 'public/build/assets/assets', // Pastikan output path sesuai
     },
 });
